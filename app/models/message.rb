2e547e9c7d4422e651e9a8aa71922f9f0eb9bec6
TomokiAbe <catttm24@gmail.com>
2016-06-14 16:13:00 +0000

    second commit message-board

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,9 @@
 class Message < ActiveRecord::Base
+    
+    #バリデーション名前必須入力、２０文字以内。
+    validates :name , length: {maximum: 20 }
+    
+    #内容は必須入力かつ２文字以上３０文字以内。
+    validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+    
 end
