609f1bf9e98968f63a0b7cbcc4997160c7b1d599
TomokiAbe <catttm24@gmail.com>
2016-06-17 13:39:49 +0000

    up4

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,12 @@
 class Message < ActiveRecord::Base
     
     #バリデーション名前必須入力、２０文字以内。
-    validates :name , length: {maximum: 20 }
+    validates :name , length: {maximum: 20 }, presence: true 
     
     #内容は必須入力かつ２文字以上３０文字以内。
-    validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+    validates :body , length: { minimum: 2, maximum: 30 }, presence: true
 
+    #年齢は0 - 120歳までとする。
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to:120 }
     
 end
