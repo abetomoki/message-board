182abd45081984b01c02472adc07a5255e1f1bcf
TomokiAbe <catttm24@gmail.com>
2016-06-17 10:54:08 +0000

    age up

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613065449) do
+ActiveRecord::Schema.define(version: 20160617102156) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
